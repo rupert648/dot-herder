55409fd7fb8e7940b98b1bccb9a56faff950fbc8
Rupert Carr <rupert@cloudflare.com>
2024-08-29 17:09:24 +0100

    fixed config format

--- src/cli.rs
+++ src/cli.rs
@@ -3,6 +3,13 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 pub struct Args {
-    #[clap(short, long, value_parser)]
+    #[clap(short, long, value_parser, help = "Path to the configuration file")]
     pub config: String,
+
+    #[clap(
+        long,
+        value_parser,
+        help = "Optional home directory path. Defaults to home (~/)"
+    )]
+    pub home: Option<String>,
 }
