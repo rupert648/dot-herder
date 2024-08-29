d77f4ef82d2f7faa4cc5b76002fb4d52b941d22f
Rupert Carr <rupert@cloudflare.com>
2024-08-29 16:06:49 +0100

    refactored code and added safety checks on repo path

---
+++ src/file_operations.rs
@@ -0,0 +1,129 @@
+use crate::config::Dotfile;
+use crate::secret::check_for_and_accept_secrets;
+use colored::*;
+use std::fs;
+use std::path::{Path, PathBuf};
+use symlink::symlink_file;
+use thiserror::Error;
+
+pub fn find_dotfiles(dotfiles: &[Dotfile]) -> Vec<PathBuf> {
+    dotfiles
+        .iter()
+        .filter_map(|dotfile| {
+            let path =
+                PathBuf::from(shellexpand::tilde(&dotfile.path).to_string()).join(&dotfile.name);
+            if path.exists() {
+                Some(path)
+            } else {
+                None
+            }
+        })
+        .collect()
+}
+
+pub fn create_dotfiles_repo(
+    repo_path: &str,
+    dotfiles: &[PathBuf],
+) -> Result<(), Box<dyn std::error::Error>> {
+    fs::create_dir_all(repo_path)?;
+
+    for dotfile in dotfiles {
+        if !check_for_and_accept_secrets(dotfile)? {
+            continue;
+        }
+
+        let target = Path::new(repo_path).join(dotfile.file_name().unwrap());
+
+        symlink_file(dotfile, &target)?;
+        println!(
+            "  {} {}",
+            "Symlinked:".green(),
+            dotfile.to_string_lossy().blue()
+        );
+    }
+
+    Ok(())
+}
+
+#[derive(Error, Debug)]
+pub enum RepoPathError {
+    #[error("The path already exists")]
+    PathAlreadyExists,
+    #[error("The parent directory does not exist")]
+    ParentDirectoryNotFound,
+    #[error("The parent directory is not writable")]
+    ParentDirectoryNotWritable,
+    #[error("Invalid path: {0}")]
+    InvalidPath(String),
+    #[error("IO error: {0}")]
+    IoError(#[from] std::io::Error),
+}
+
+pub fn is_valid_repo_path(path: &str) -> Result<(), RepoPathError> {
+    let path = Path::new(path);
+
+    // Check if the path already exists
+    if path.exists() {
+        return Err(RepoPathError::PathAlreadyExists);
+    }
+
+    // Check if the parent directory exists and is writable
+    let parent = path
+        .parent()
+        .ok_or_else(|| RepoPathError::InvalidPath("Path has no parent directory".to_string()))?;
+
+    if !parent.exists() {
+        return Err(RepoPathError::ParentDirectoryNotFound);
+    }
+
+    if !parent.is_dir() {
+        return Err(RepoPathError::InvalidPath(
+            "Parent is not a directory".to_string(),
+        ));
+    }
+
+    // Check if the parent directory is writable by attempting to create a temporary file
+    let temp_file_path = parent.join(".tmp_write_test");
+    match fs::File::create(&temp_file_path) {
+        Ok(_) => {
+            let _ = fs::remove_file(temp_file_path);
+            Ok(())
+        }
+        Err(_) => Err(RepoPathError::ParentDirectoryNotWritable),
+    }
+}
+
+pub fn print_file_tree(dir: &Path, level: usize) -> std::io::Result<()> {
+    if dir.is_dir() {
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
+            let path = entry.path();
+
+            if path.file_name().unwrap() == ".git" {
+                continue;
+            }
+
+            let prefix = if level == 0 {
+                "".to_string()
+            } else {
+                format!("{}└─ ", "   ".repeat(level - 1))
+            };
+
+            if path.is_dir() {
+                println!(
+                    "{}{}/",
+                    prefix.blue(),
+                    path.file_name().unwrap().to_string_lossy().cyan()
+                );
+                print_file_tree(&path, level + 1)?;
+            } else {
+                println!(
+                    "{}{}",
+                    prefix.blue(),
+                    path.file_name().unwrap().to_string_lossy().green()
+                );
+            }
+        }
+    }
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,31 @@
+mod cli;
+mod config;
+mod file_operations;
+mod git_operations;
 mod secret;
+mod tui;
 
 use clap::Parser;
+use cli::Args;
 use colored::*;
-use dialoguer::{Input, MultiSelect};
-use secret::check_for_and_accept_secrets;
-use serde::{Deserialize, Serialize};
+use config::Config;
+use file_operations::{create_dotfiles_repo, find_dotfiles, print_file_tree};
+use git_operations::init_git_repo;
 use std::fs;
-use std::path::{Path, PathBuf};
-use std::process::Command;
-use symlink::symlink_file;
-
-#[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
-struct Args {
-    #[clap(short, long, value_parser)]
-    config: String,
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-struct Dotfile {
-    name: String,
-    path: String,
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-struct DotfilesList {
-    dotfiles: Vec<Dotfile>,
-}
+use std::path::Path;
+use tui::{get_repo_path, select_dotfiles};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
     let config = fs::read_to_string(args.config)?;
-    let dotfiles_list: DotfilesList = serde_yaml::from_str(&config)?;
+    let config: Config = serde_yaml::from_str(&config)?;
 
     println!("{}", "Welcome to Dotfiles Manager!".green().bold());
 
-    let repo_path: String = Input::new()
-        .with_prompt("Enter the path for your .dotfiles repo".cyan().to_string())
-        .interact_text()?;
+    let repo_path = get_repo_path()?;
 
     println!("{}", "Searching for dotfiles...".yellow());
-    let found_dotfiles = find_dotfiles(&dotfiles_list.dotfiles);
+    let found_dotfiles = find_dotfiles(&config.dotfiles);
 
     println!("{}", "Found the following dotfiles:".green());
     for dotfile in &found_dotfiles {
@@ -51,111 +36,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     println!("{}", "Creating dotfiles repo...".yellow());
     create_dotfiles_repo(&repo_path, &selected_dotfiles)?;
+    println!("{}", "Initialising git...".yellow());
+    init_git_repo(&repo_path)?;
 
     println!("{}", "Dotfiles repo created successfully!".green().bold());
     println!("Repo location: {}\n", repo_path.cyan());
 
     print_file_tree(Path::new(&repo_path), 0)?;
 
-    Ok(())
-}
-
-fn find_dotfiles(dotfiles: &[Dotfile]) -> Vec<PathBuf> {
-    dotfiles
-        .iter()
-        .filter_map(|dotfile| {
-            let path =
-                PathBuf::from(shellexpand::tilde(&dotfile.path).to_string()).join(&dotfile.name);
-            if path.exists() {
-                Some(path)
-            } else {
-                None
-            }
-        })
-        .collect()
-}
-
-fn select_dotfiles(dotfiles: &[PathBuf]) -> Result<Vec<PathBuf>, Box<dyn std::error::Error>> {
-    let dotfiles_strings: Vec<String> = dotfiles
-        .iter()
-        .map(|p| p.to_string_lossy().into_owned())
-        .collect();
-
-    let selections = MultiSelect::new()
-        .with_prompt("Select dotfiles to include".cyan().to_string())
-        .items(&dotfiles_strings)
-        .defaults(&vec![true; dotfiles.len()])
-        .interact()?;
-
-    Ok(selections
-        .into_iter()
-        .map(|i| dotfiles[i].clone())
-        .collect())
-}
-
-fn create_dotfiles_repo(
-    repo_path: &str,
-    dotfiles: &[PathBuf],
-) -> Result<(), Box<dyn std::error::Error>> {
-    fs::create_dir_all(repo_path)?;
-
-    for dotfile in dotfiles {
-        if !check_for_and_accept_secrets(dotfile)? {
-            continue;
-        }
-
-        let target = Path::new(repo_path).join(dotfile.file_name().unwrap());
-
-        symlink_file(dotfile, &target)?;
-        println!(
-            "  {} {}",
-            "Symlinked:".green(),
-            dotfile.to_string_lossy().blue()
-        );
-    }
-
-    println!("{}", "Initializing git repository...".yellow());
-    Command::new("git")
-        .arg("init")
-        .current_dir(repo_path)
-        .output()?;
-
-    println!("{}", "Git repository initialized.".green());
-
-    Ok(())
-}
-
-fn print_file_tree(dir: &Path, level: usize) -> std::io::Result<()> {
-    if dir.is_dir() {
-        for entry in fs::read_dir(dir)? {
-            let entry = entry?;
-            let path = entry.path();
-
-            if path.file_name().unwrap() == ".git" {
-                continue;
-            }
-
-            let prefix = if level == 0 {
-                "".to_string()
-            } else {
-                format!("{}└─ ", "   ".repeat(level - 1))
-            };
-
-            if path.is_dir() {
-                println!(
-                    "{}{}/",
-                    prefix.blue(),
-                    path.file_name().unwrap().to_string_lossy().cyan()
-                );
-                print_file_tree(&path, level + 1)?;
-            } else {
-                println!(
-                    "{}{}",
-                    prefix.blue(),
-                    path.file_name().unwrap().to_string_lossy().green()
-                );
-            }
-        }
-    }
     Ok(())
 }
